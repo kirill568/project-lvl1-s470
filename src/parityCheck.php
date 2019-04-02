e0596b783f5eb281bebae587f31679225cff84c9
kirill <kirillbalaev568@gmail.com>
2019-04-02 16:18:51 +0700

    correctedLinterErrors

--- src/parityCheck.php
+++ src/parityCheck.php
@@ -5,36 +5,36 @@ namespace BrainGames\parityCheck;
 use function \cli\line;
 use function \cli\prompt;
 
-function randomNumber ($min = 1, $max = 30)
+function randomNumber($min = 1, $max = 30)
 {
-	return rand($min, $max);
+    return rand($min, $max);
 }
 
-function parity ($number)
+function parity($number)
 {
-	return $number % 2 === 0 ? 'yes' : 'no';
+    return $number % 2 === 0 ? 'yes' : 'no';
 }
 
-function runGame ()
+function runGame()
 {
-	line('Welcome to the Brain Games!');
-	line('Answer "yes" if number even otherwise answer "no".');
-    $name = prompt('May I have your name?');
-    line("Hello , {$name}");
-	$question = 1;
-	while ($question <= 3) {
-		$currentNumber = randomNumber();
-		$parity = parity($currentNumber);
-		line("Question: {$currentNumber}");
-		$answer = prompt("Your answer");
-		if ($answer === $parity) {
-			line('Correct!');
-		} else {
-			line("'{$answer}' is now wrong answer ;(. Correct answer was '{$parity}'");
-			line("Let's try again, {$name}");
-			return;
-		}
-		$question += 1;
-	}
-	line("Congratulations, {$name}!");
+    line('Welcome to the Brain Games!');
+    line('Answer "yes" if number even otherwise answer "no".');
+       $name = prompt('May I have your name?');
+       line("Hello , {$name}");
+    $question = 1;
+    while ($question <= 3) {
+        $currentNumber = randomNumber();
+        $parity = parity($currentNumber);
+        line("Question: {$currentNumber}");
+        $answer = prompt("Your answer");
+        if ($answer === $parity) {
+            line('Correct!');
+        } else {
+            line("'{$answer}' is now wrong answer ;(. Correct answer was '{$parity}'");
+            line("Let's try again, {$name}");
+            return;
+        }
+        $question += 1;
+    }
+    line("Congratulations, {$name}!");
 }
