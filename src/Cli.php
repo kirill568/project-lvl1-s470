dc7a05e94484b2ca5dd5a733c8251d1a6f69af17
kirill <kirillbalaev568@gmail.com>
2019-04-02 14:57:25 +0700

    refactoringStepSecond

--- src/Cli.php
+++ src/Cli.php
@@ -6,6 +6,7 @@ use function \cli\line;
 use function \cli\prompt;
 function run()
 {
+	line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello , {$name}");
 }
