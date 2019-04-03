a1fc166fd20f1254c82652b5db26c964f493127a
kirill <kirillbalaev568@gmail.com>
2019-04-03 16:25:01 +0700

    addNewGame

---
+++ src/games/brain-calc.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BrainGames\brainCalc;
+
+use function BrainGames\flow\engine;
+
+function getOperationAndResult ($firstNumber, $secondNumber)
+{
+	$operations = [
+	['+' => $firstNumber + $secondNumber], 
+	['-' => $firstNumber - $secondNumber],
+	['*' => $firstNumber * $secondNumber]
+	];
+	$operation = rand(0, 2);
+	return $operations[$operation];
+}
+
+function runGame()
+{
+    $questions = [];
+    $answers = [];
+    $rule = 'What is the result of the expression?';
+    $questionsNumber = 3;
+    for ($i = 1; $i <= $questionsNumber; $i += 1) {
+        $firstNumber = rand(1, 30);
+        $secondNumber = rand(1, 30);
+        $operationAndResult = getOperationAndResult($firstNumber, $secondNumber);
+        $operation = key($operationAndResult);
+        $answer = $operationAndResult[$operation];
+        $question = "{$firstNumber} {$operation} {$secondNumber}";
+        $questions[] = $question;
+        $answers[] = "{$answer}";
+    }
+    engine($answers, $questions, $rule);
+}
---
+++ src/games/brain-even.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrainGames\brainEven;
+
+use function \cli\line;
+use function \cli\prompt;
+use function BrainGames\flow\engine;
+
+function isParity($number)
+{
+    return $number % 2 === 0;
+}
+
+function runGame()
+{
+    $questionsNumber = 3;
+    $questions = [];
+    $answers = [];
+    $rule = 'Answer "yes" if number even otherwise answer "no".';
+    for ($i = 1; $i <= $questionsNumber; $i += 1) { 
+        $question = rand(1, 30);
+        $answer = isParity($question) ? 'yes' : 'no';
+        $questions[] = $question;
+        $answers[] = $answer;
+    }
+    engine($answers, $questions, $rule);
+}
