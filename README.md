9f228ef81b98b413391c4ba5869eb56bda0334d8
kirill <kirillbalaev568@gmail.com>
2019-04-04 17:11:01 +0700

    updateReadme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/178f44023b0768f5c1d8/maintainability)](https://codeclimate.com/github/kirill568/project-lvl1-s470/maintainability)
 [![Build Status](https://travis-ci.org/kirill568/project-lvl1-s470.svg?branch=master)](https://travis-ci.org/kirill568/project-lvl1-s470)
 
+[![asciicast](https://asciinema.org/a/wJAyn5AmXqY1r1kZiyPomRCES.svg)](https://asciinema.org/a/wJAyn5AmXqY1r1kZiyPomRCES)
+
 [![asciicast](https://asciinema.org/a/lGaWKxEBT9GBlj5R8fi6vAZ1v.svg)](https://asciinema.org/a/lGaWKxEBT9GBlj5R8fi6vAZ1v)
--- src/games/brain-gcd.php
+++ src/games/brain-gcd.php
@@ -7,28 +7,28 @@ use const BrainGames\flow\QUESTIONS_NUMBER;
 
 const DESCRIPTION = "Find the greatest common divisor of given numbers.";
 
-function getGcd ($firstNumber, $secondNumber)
+function getGcd($firstNumber, $secondNumber)
 {
-	while ($firstNumber !== 0 && $secondNumber !== 0) {
-		if ($firstNumber > $secondNumber) {
-			$firstNumber = $firstNumber % $secondNumber;
-		} else {
-			$secondNumber = $secondNumber % $firstNumber;
-		}
-	}
-	return $firstNumber + $secondNumber;
+    while ($firstNumber !== 0 && $secondNumber !== 0) {
+        if ($firstNumber > $secondNumber) {
+            $firstNumber = $firstNumber % $secondNumber;
+        } else {
+            $secondNumber = $secondNumber % $firstNumber;
+        }
+    }
+    return $firstNumber + $secondNumber;
 }
 
-function runGame ()
+function runGame()
 {
     $questionsAnswers = [];
-    for ($i = 1; $i <= QUESTIONS_NUMBER; $i +=1 ) {
-    	$firstNumber = rand(1, 30);
-    	$secondNumber = rand(1, 30);
-    	$gcd = getGcd($firstNumber, $secondNumber);
-    	$question = "{$firstNumber} {$secondNumber}";
-    	$answer = "{$gcd}";
-    	$questionsAnswers[] = [$question, $answer];
+    for ($i = 1; $i <= QUESTIONS_NUMBER; $i += 1) {
+        $firstNumber = rand(1, 30);
+        $secondNumber = rand(1, 30);
+        $gcd = getGcd($firstNumber, $secondNumber);
+        $question = "{$firstNumber} {$secondNumber}";
+        $answer = "{$gcd}";
+        $questionsAnswers[] = [$question, $answer];
     }
     engine($questionsAnswers, DESCRIPTION);
 }
